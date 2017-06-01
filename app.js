02ebd73377cc93a938efc36058828f1bb6b2a821
rvdpas <rvdpas94@gmail.com>
2017-06-01 09:17:38 +0200

    add sockets

--- app.js
+++ app.js
@@ -1,43 +1,64 @@
 var express = require('express');
-var request = require('request');
+var http = require('http');
 var bodyParser = require('body-parser');
 var path = require('path');
 var compression = require('compression');
+var socketio = require('socket.io');
 
 var app = express();
+var server = http.createServer(app);
+var io = socketio.listen(server);
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+users = [];
+connections = [];
 
-// body parser middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
-// set static path
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Global vars
-app.use(function(req, res, next) {
-	res.locals.errors = null;
-	next();
+server.listen(3000, function() {
+  console.log("Server started on port 3000...");
 });
 
-// Compression
-app.use(compression({
-  threshold: 0,
-  filter: () => true,
-}));
+app.set('view engine', 'ejs' );
 
-// Overview Page
-app.get('/', function (req, res) {
-// Get your data here, like an api request
-})
+app.set('views', path.join(__dirname, 'views'));
 
-// Detail page
-app.get('/detail', function (req, res) {
-// Get your data here, like an api request
-})
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, function() {
-	console.log("Server started on port 3000...");
-})
+app.get('/', function(req, res){
+  res.render('index.ejs');
+});
+
+io.sockets.on('connection', function(socket) {
+  connections.push(socket);
+  console.log('Connected: %s sockets connected', connections.length);
+
+  // Disconnect
+  socket.on('disconnect', function(data) {
+    users.splice(users.indexOf(socket.username), 1);
+    updateUsernames();
+
+    connections.splice(connections.indexOf(socket), 1);
+    console.log('Disconnected: %s sockets connected', connections.length);
+  });
+
+  // Send message
+  socket.on('send message', function(data) {
+    io.sockets.emit('new message', {
+      msg: data,
+      user: socket.username
+    });
+    console.log(data);
+  });
+
+  // New user
+  socket.on('new user', function(data, callback) {
+    callback(true);
+    socket.username = data;
+    users.push(socket.username);
+    updateUsernames();
+    console.log(socket.username);
+  });
+
+  // Update users
+  function updateUsernames() {
+    io.sockets.emit('get users', users);
+  };
+});
