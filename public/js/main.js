b26d0cb60df3f2a58f786c43421d65fa3c66f35f
rvdpas <rvdpas94@gmail.com>
2017-06-21 16:41:28 +0200

    update data and add/organize partials

--- public/js/main.js
+++ public/js/main.js
@@ -1,37 +1,3 @@
-// document.getElementById("getInterests").addEventListener("click", function() {
-
-//   var showInterests = document.querySelectorAll(".hide");
-//   [].forEach.call(showInterests, function(el) {
-//     el.classList.remove("hide");
-//   });
-
-//   var interests = document.getElementById("interests");
-//   interests.classList.add("hide");
-// });
-
-
-// function getCheckedCheckboxesFor(checkboxName) {
-//     var checkboxes = document.querySelectorAll('input[name="' + checkboxName + '"]:checked'), values = [];
-//     Array.prototype.forEach.call(checkboxes, function(el) {
-//         values.push(el.value);
-//     });
-//   return values;
-// }
-
-// function toggleClass(el) {
-//   if(el.className == "hide") {
-//     el.className = "";
-//   } else {
-//     el.className = "hide";
-//   }
-// }
-
-// var checkbox = document.getElementsByClassName(".toggle").addEventListener("change", function(elem){
-//   checkBox.checked = false;
-//   elem.style.display = this.checked ? 'block' : 'none';
-// });
-// checkBox.onchange();
-
 // Toggle checkbox and show associated content
 var checkboxes = document.querySelectorAll('input[name="interest"]');
 checkboxes.forEach(function(el) {
